5aa65019bb99a6c59e0935e3af6881c90be175d0
thareesha2006 <118061399+thareesha2006@users.noreply.github.com>
2022-11-13 21:03:49 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,7 +30,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
 global.vcardowner = ['94772550280'] // owner number
-global.ownername = "😎Noti boy" // owner name
+global.ownername = "😈චුට්ටා😈" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
